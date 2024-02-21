ebce2bad1706e2d386057f760164e9700a435cee
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-02-21 23:45:44 +0530

    add GeneralForm component

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -1,9 +1,16 @@
+import GeneralForm from './GeneralFrom';
+
 function From({ state, formHandlers }) {
   
   return (
     <>
       <div className='form'>
-        
+        <GeneralForm
+          name={state.name}
+          phone={state.phone}
+          email={state.email}
+          formHandlers={formHandlers}
+        />
       </div>
     </>
   );
---
+++ src/components/Forms/GeneralFrom.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import lock from '/lock.svg';
+import unlock from '/unlock.svg';
+import '../../styles/GeneralForm.css';
+
+function GeneralForm({ name, phone, email, formHandlers }) {
+  const [isActive, setIsActive] = useState(true);
+  return (
+    <>
+      {isActive ? (
+        <div className='general'>
+          <div className='name'>
+            <label htmlFor='name'>Name: </label>
+            <input
+              type='text'
+              id='name'
+              value={name}
+              onChange={formHandlers.changeName}
+            />
+          </div>
+
+          <div className='email'>
+            <label htmlFor='email'>Email: </label>
+            <input
+              type='email'
+              id='email'
+              value={email}
+              onChange={formHandlers.changeEmail}
+            />
+          </div>
+
+          <div className='phone'>
+            <label htmlFor='phone'>Phone: </label>
+            <input
+              type='tel'
+              id='phone'
+              value={phone}
+              onChange={formHandlers.changePhone}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className='lock general'>
+          <div>Name: {name}</div>
+          <div>Email: {email}</div>
+          <div>Phone: {phone}</div>
+        </div>
+      )}
+      <button onClick={() => setIsActive(!isActive)}>
+        {isActive ? 'Save' : 'Edit'}
+        {isActive ? <img src={unlock} alt='save' /> : <img src={lock} alt='edit' />}
+      </button>
+    </>
+  );
+}
+
+export default GeneralForm;
