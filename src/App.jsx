c065afe17bcac4f5ba4e6d624e61855988169124
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-02-22 23:27:33 +0530

    add: Navbar component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Main from './components/Main';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Main />
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import '../styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <>
+      <h1>CV Builder</h1>
+    </>
+  );
+}
+
+export default Navbar;
