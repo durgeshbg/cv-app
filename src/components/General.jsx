8438b6632cac3a9639ee6dc4f3101c035df68089
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-02-26 23:10:06 +0530

    add: General styles

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,10 +1,21 @@
+import nameSVG from '../assets/name.svg';
+import emailSVG from '../assets/email.svg';
+import phoneSVG from '../assets/phone.svg';
+import '../styles/General.css';
 function General({ name, email, phone }) {
   return (
     <>
       <div className='general'>
-        <div>Name: {name}</div>
-        <div>Email: {email}</div>
-        <div>Phone: {phone}</div>
+        <div className='name'>
+          <img src={nameSVG} alt='name svg' />
+          {name}
+        </div>
+        <div className='email'>
+          <img src={emailSVG} alt='email svg' /> {email}
+        </div>
+        <div className='phone'>
+          <img src={phoneSVG} alt='phone svg' /> {phone}
+        </div>
       </div>
     </>
   );
